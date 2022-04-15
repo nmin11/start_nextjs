8953e5fc196bbf7459615119b2b6a68324baf19c
nmin11 <nmin1124@gmail.com>
2022-04-15 14:47:15 +0900

    Finish : #1 framework overview

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,12 +4,22 @@ import { useRouter } from 'next/router'
 export default function NavBar() {
 	const router = useRouter();
 
-	return <nav>
-		<Link href="/">
-			<a style={{color: router.pathname === "/" ? "red" : "blue"}}>Home</a>
-		</Link>
-		<Link href="/about">
-			<a style={{color: router.pathname === "/about" ? "red" : "blue"}}>About</a>
-		</Link>
-	</nav>
+	return (
+		<nav>
+			<Link href="/">
+				<a className={router.pathname === "/" ? "active" : ""}>Home</a>
+			</Link>
+			<Link href="/about">
+				<a className={router.pathname === "/about" ? "active" : ""}>About</a>
+			</Link>
+			<style jsx>{`
+				a {
+					text-decoration: none;
+				}
+				.active {
+					color: tomato;
+				}
+			`}</style>
+		</nav>
+	);
 }
